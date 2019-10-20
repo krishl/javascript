5e664c2783b97e54eefe58a54cb34106f1976614
Krishel Lasam <krishel.lasam@gmail.com>
2019-10-20 16:33:31 -0400

    Create exercise-5-quick-sort-solution.js

---
+++ four-semesters-of-computer-science-in-5-hours/exercise-5-quick-sort-solution.js
@@ -0,0 +1,48 @@
+/*
+
+  Quicksort!
+  
+  Name your function quickSort.
+  
+  Quicksort should grab a pivot from the end and then separate the list (not including the pivot)
+  into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
+  lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
+  list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
+  is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
+  
+  As always, you can change describe to xdescribe to prevent the unit tests from running while you're coding.
+  
+  No visualization is provided so feel free to use your own debugging methods (like console.log).
+
+*/
+
+const quickSort = nums => {
+  if (nums.length <= 1) return nums;
+  
+  const pivot = nums[nums.length-1];
+  const left = [];
+  const right = [];
+  
+  for (let i = 0; i < nums.length-1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    }
+    else {
+      right.push(nums[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+
+// unit tests
+// do not modify the below code
+describe('quickSort', function() {
+  it('quicksort an array', () => {
+    const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
+    const answer = quickSort(input);
+    
+    expect(answer).toEqual([1,2,3,4,5,6,7,8,9,10]);
+
+  });
+});
