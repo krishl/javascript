3c5b543048043ac116dbebe5a4762861e1595863
Krishel Lasam <krishel.lasam@gmail.com>
2019-10-28 12:08:09 -0400

    Create recursive-factorial-and-memoize-exercise.js

---
+++ a-practical-guide-to-algorithms-wth-javascript/recursive-factorial-and-memoize-exercise.js
@@ -0,0 +1,28 @@
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+const factorial = memoize(
+  (x) => {
+    if (x === 0) {
+      return 1;
+    }
+    else {
+      return x * factorial(x - 1);
+    }
+  }
+);
+
+console.log(factorial(5)); // calculated
+console.log(factorial(5)); // calculated for 6 and cached for 5
