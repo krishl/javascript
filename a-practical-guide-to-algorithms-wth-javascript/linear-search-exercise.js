455aac26f83997d6c8137ae23a814db9959778e9
Krishel Lasam <krishel.lasam@gmail.com>
2019-10-29 14:53:26 -0400

    Create linear-search-exercise.js

---
+++ a-practical-guide-to-algorithms-wth-javascript/linear-search-exercise.js
@@ -0,0 +1,13 @@
+// TASK: Implement linear search.
+
+function linearSearch(list, item) {
+  let index = -1;
+  list.forEach((listItem, i) => {
+    if (listItem === item) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+linearSearch([2,6,7,90,103], 90);
