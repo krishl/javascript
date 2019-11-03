51049cdeaf6c74995b53877ddf75f0c404d4bd1e
Krishel Lasam <krishel.lasam@gmail.com>
2019-11-03 17:23:28 -0500

    Create linked-list-exercise.js

---
+++ introduction-to-data-structures-for-interviews/linked-list-exercise.js
@@ -0,0 +1,95 @@
+/** Class representing a Linked List */
+
+class LinkedList {
+
+  constructor(value) {
+    this.head = {value, next: null};
+    this.tail = this.head;
+  }
+  /*
+  * Inserts a new value to the end of the linked list
+  * @param {*} value - the value to insert
+  */
+  insert(value) {
+    const node = {value, next: null};
+    this.tail.next = node;
+    this.tail = node;
+  }
+  /*
+  * Removes the value at the end of the linked list
+  */
+// {
+//   head: {value: 1, next: {value: 2, next: null}}
+//   tail: {value: 2, next: null}
+// }
+  removeTail() {
+    let currentNode = this.head;
+
+    while (currentNode.next !== this.tail) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = null;
+    this.tail = currentNode;
+  }
+  /*
+  * Searches the linked list and returns true if it contains the value passed
+  * @param {*} value - the value to search for
+  * @return {boolean} - true if value is found, otherwise false
+  */
+  contains(value) {
+   let currentNode = this.head;
+   while (currentNode.value !== value) {
+     if (currentNode.next) {
+       currentNode = currentNode.next;
+     } else {
+       return false
+     }
+   } 
+   return true
+  }  
+  /*
+  * Checks if a node is the head of the linked list 
+  * @param {{prev:Object|null, next:Object|null}} node - the node to check
+  * @return {boolean} - true if node is the head, otherwise false
+  */
+  isHead(node) {
+    return this.head === node;
+  }
+  /*
+  * Checks if a node is the tail of the linked list 
+  * @param {{prev:Object|null, next:Object|null}} node - the node to check
+  * @return {boolean} - true if node is the tail, otherwise false
+  */
+  isTail(node) {
+    return this.head === node;
+  }
+}
+
+const myList = new LinkedList(1); //initiate
+
+
+// {
+//   head: {value: 1, next: null}
+//   tail: {value: 1, next: null}
+// }
+
+myList.insert(2)
+
+// {
+//   head: {value: 1, next: {value: 2, next: null}}
+//   tail: {value: 2, next: null}
+// }
+
+myList.insert(3)
+
+
+// {
+//   head: {value: 1, next: {value: 2, next: {value: 3, next: null}}}
+//   tail: {value: 3, next: null}
+// }
+
+console.log(myList);
+myList.removeTail();
+console.log(myList);
+myList.contains(1);
